d70f4bb11283a60220d99632217556c6ce00840a
dsfgsdfgf <kaanixir@gmail.com>
2017-05-29 17:46:54 +1000

    r3x alpha v0.001

---
+++ js/kaan.js
@@ -0,0 +1,109 @@
+
+
+var ticker_btcusd_url = 'https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD';
+var request = require('request');
+var client = new XMLHttpRequest();
+let hget = require('hget');
+let marked = require('marked');
+
+
+console("DUDE SUPPPP");
+
+
+
+
+
+
+
+$(function() {
+    var  bgimage = new Image();
+    bgimage.url="img/bg2.jpg";
+    $("#_bg").hide();
+    $(bgimage).load(function() {
+        $("#_bg").css("background","url(img/bg2.jpg) center;").fadeIn(9000);
+    });
+})
+
+
+
+
+// On 'ENTER' ,     <     <    <   <  < < ! GO ! > >  >   >    >     >
+$(document).keypress(function (e) {
+    if (e.which == 13 || event.keyCode == 13) {
+        alert(' <     <    <   <  < < ! GO ! > >  >   >    >     >');
+    }
+});
+
+
+
+
+
+
+
+// The Glow...
+setInterval(function () {
+    $('#_2').addClass('jackInTheBox');
+}, 1000);
+
+
+// TADAAAAA
+
+setInterval(function () {
+    // $('#_0').toggleClass('jackInTheBox');
+}, 1000);
+
+
+
+
+
+
+//            $("_10").ajax()
+
+
+// void read();
+async function read () {
+    var html = await getRandomPonyFooArticle();
+    var md = hget(html, {
+        markdown: true,
+        root: 'main',
+        ignore: '.at-subscribe,.mm-comments,.de-sidebar'
+    });
+    var txt = marked(md, {
+        renderer: new Term()
+    });
+    console.log(txt);
+}
+
+
+
+function handler() {
+    var tmp = this.responseXML.getElementById('').textContent;
+    if(this.status == 200 && this.responseXML != null && tmp) {
+        // success!
+        $('_10').textContent = tmp;
+    } else {        // Zebugging purposes
+        alert("buddybooo handler() : FAIL");
+        // or ?
+        console.log("buddybooo handler() : FAIL");
+    }
+}
+
+// To log a message to server (Go backend, <<    /log  API   >> )
+function log(message) {
+    client.open("POST", "/log");
+    client.setRequestHeader("Content-Type", "text/plain;charset=UTF-8");
+    client.send(message);
+}
+
+
+client.onload = handler;
+client.open("GET", ticker_btcusd_url);
+client.send();
+request.get(
+    `https://api.bitfinex.com/v2/tickers/ticker/tBTCUSD`,
+    (error, response, body) => console.log(body)
+)
+
+
+
+
